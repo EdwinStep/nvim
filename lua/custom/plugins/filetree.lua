e8553b9e92673ac88a15935ea2147aa376daa55d
Edvinas Steponavičius <steponaviciusedvinas@gmail.com>
2023-11-07 23:10:51 +0200

    Fixed autoindent config, added bufferline plugin, added keymap for toggling Neotree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -6,7 +6,8 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
+  config = function()
     require('neo-tree').setup {}
+    vim.keymap.set('n', '<leader>nt', '<Cmd>Neotree toggle<CR>')
   end,
 }

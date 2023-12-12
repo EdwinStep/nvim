7394051184cf5694994cb97a5f65ab98ffeeae27
Edvinas Steponavičius <edvinas.steponaviciu@roxit.nl>
2023-12-12 10:11:49 +0200

    Neotest

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -8,9 +8,57 @@ return {
   config = function()
     require("neotest").setup({
       adapters = {
-        require("neotest-playwright")
+        require("neotest-playwright").adapters({
+          options = {
+            enable_dynamic_test_discovery = false,
+            preset = "none",
+          }
+        }),
       },
+      consumers = {
+        playwright = require("neotest-playwright.consumers").consumers,
+      }
     })
-    vim.keymap.set('n', '<leader>tr', require("neotest").run.run())
-  end
+  end,
+  keys = {
+    { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+    {
+      "<leader>tT",
+      function() require("neotest").run.run(vim.loop.cwd()) end,
+      desc =
+      "Run All Test Files"
+    },
+    {
+      "<leader>tr",
+      function() require("neotest").run.run() end,
+      desc =
+      "Run Nearest"
+    },
+    {
+      "<leader>ts",
+      function() require("neotest").summary.toggle() end,
+      desc =
+      "Toggle Summary"
+    },
+    {
+      "<leader>to",
+      function() require("neotest").output.open({ enter = true, auto_close = true }) end,
+      desc =
+      "Show Output"
+    },
+    {
+      "<leader>tO",
+      function() require("neotest").output_panel.toggle() end,
+      desc =
+      "Toggle Output Panel"
+    },
+    { "<leader>tS", function() require("neotest").run.stop() end,                  desc = "Stop" },
+    {
+      "<leader>ta",
+      function()
+        require("neotest").playwright.attachment()
+      end,
+      desc = "Launch test attachment",
+    }
+  }
 }
